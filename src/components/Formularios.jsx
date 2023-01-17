0b269623594a52fde05292fe772f8c2897716b15
SebitasRojas2023 <sebascamilorojas77@gmail.com>
2023-01-17 18:05:22 -0500

    hola

---
+++ src/components/Formularios.jsx
@@ -0,0 +1,26 @@
+
+const Formularios = () => {
+  
+    return (
+    <div className="md:w-1/2  lg:w-2/5">
+        <h2 className="font-black  text-3xl text-center">
+         Seguimiento a Pacientes
+        
+      </h2>
+       <p className="text-lg mt-5 text-center">
+         Añade Pacientes{' '}
+         <span className="text-indigo-600 font-bold">Administrarlos</span>
+      </p>
+        <form action="">
+        
+        </form>
+
+      </div>
+    ) 
+     
+    
+       
+  }
+
+
+export default Formularios
