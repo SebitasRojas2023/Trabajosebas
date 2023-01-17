55ffa9ec60a92c0404fcd9d4e424fdcb24da5be5
SebitasRojas2023 <sebascamilorojas77@gmail.com>
2023-01-17 18:04:36 -0500

    Actualizar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 
+/*importar modulos*/
+import Formulario from "./components/Formularios"
+import Header from "./components/Header"
+import ListadoPacientes from "./components/ListadoPacientes"
 
+
+/*Funcion creadora de la apliacion*/ 
 function App() {
+return (
+ /* Etiqueta padre y el body de la pagina*/  
 
 
-  return (
-    <>
-    
-    </>
+   <div className="container mx-auto mt-20">
+   <Header/>
+   <div className="mt-12 md:flex">
+     <Formulario />
+     <ListadoPacientes />
+    </div>
+    </div>
   )
-}
-
+ }
+/* Export  para importar el modulo a otras partes de la app*/
 export default App
