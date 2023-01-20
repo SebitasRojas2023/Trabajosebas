f9aa9bb367481d54f10e8334b22deb81d8d5f693
SebitasRojas2023 <sebascamilorojas77@gmail.com>
2023-01-20 18:58:06 -0500

    savecommit

--- src/components/Pacientes.jsx
+++ src/components/Pacientes.jsx
@@ -1,21 +1,27 @@
+const Pacientes = () => {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl mx-5 my-10">
+        <p className="font-bold mb-3 text-gray-700 uppercase">
+          Nombre:{' '} <span className="font-normal normal-case">Trosky</span>
+        </p>
 
-  const Pacientes = () => {
-    return (
-      <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl m-5 my-10">
-          <p className="font-bold mb-3 text-gray-700 uppercase">Nombre:{" "} <span className='font-normal normal-case'>Rocko</span></p>
-          <p className="font-bold mb-3 text-gray-700 uppercase">Propietario:{" "} <span className='font-normal normal-case'>Sebastian Camilo Rojas Polania</span></p>
-          <p className="font-bold mb-3 text-gray-700 uppercase">Email:{" "} <span className='font-normal normal-case'>sc.rojas_2019-1@corhuila.edu.co</span></p>
-          <p className="font-bold mb-3 text-gray-700 uppercase">Fecha de ingreso:{" "} <span className='font-normal normal-case'>17 de enero de 2023</span></p>
-          <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas:{" "} <span className='font-normal normal-case'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque ex, exercitationem rerum et sit nemo explicabo labore delectus suscipit reiciendis? Fugit debitis corporis placeat rem consequatur sequi omnis architecto commodi a non magni, accusantium dolorem iste assumenda accusamus eum officia dicta et quasi quos aliquid odio exercitationem nisi repellendus. Ducimus?</span></p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">
+          Propietario:{' '} <span className="font-normal normal-case">Sebastian Camilo Rojas Polania</span>
+        </p>
 
-  
-  
-  
-  
-  
-  
-        </div>
-    )
-  }
+        <p className="font-bold mb-3 text-gray-700 uppercase">
+          Email:{' '} <span className="font-normal normal-case">sc.rojas_2019-1@corhuila.edu.co</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">
+          Fecha de Ingreso:{' '} <span className="font-normal normal-case">17 de Enero de 2023</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">
+          Síntomas:{' '} <span className="font-normal normal-case">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia doloribus vero libero fugit animi aliquam tempora, ducimus nulla quibusdam ut velit consectetur magni possimus voluptatem blanditiis maxime minus voluptas ipsa neque? Temporibus quaerat natus excepturi saepe vero! Cum, ut error?</span>
+        </p>
+      </div>
+  )
+}
 
 export default Pacientes
