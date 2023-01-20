76d7a14fbe1dced55159fe05f0cb6c252e2deb1f
SebitasRojas2023 <sebascamilorojas77@gmail.com>
2023-01-20 18:51:59 -0500

    actualizar

--- src/components/Pacientes.jsx
+++ src/components/Pacientes.jsx
@@ -1,13 +1,13 @@
 
   const Pacientes = () => {
     return (
-      <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+      <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl m-5 my-10">
           <p className="font-bold mb-3 text-gray-700 uppercase">Nombre:{" "} <span className='font-normal normal-case'>Rocko</span></p>
           <p className="font-bold mb-3 text-gray-700 uppercase">Propietario:{" "} <span className='font-normal normal-case'>Sebastian Camilo Rojas Polania</span></p>
           <p className="font-bold mb-3 text-gray-700 uppercase">Email:{" "} <span className='font-normal normal-case'>sc.rojas_2019-1@corhuila.edu.co</span></p>
           <p className="font-bold mb-3 text-gray-700 uppercase">Fecha de ingreso:{" "} <span className='font-normal normal-case'>17 de enero de 2023</span></p>
           <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas:{" "} <span className='font-normal normal-case'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque ex, exercitationem rerum et sit nemo explicabo labore delectus suscipit reiciendis? Fugit debitis corporis placeat rem consequatur sequi omnis architecto commodi a non magni, accusantium dolorem iste assumenda accusamus eum officia dicta et quasi quos aliquid odio exercitationem nisi repellendus. Ducimus?</span></p>
-  
+
   
   
   
